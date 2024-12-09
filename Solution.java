cfde1327f4bc9471ebf3907c101b9e3542f9c7ce
GVOne-blood <dta150904@gmail.com>
2024-12-09 23:50:48 +0700

    L2490

--- Solution.java
+++ Solution.java
@@ -5,6 +5,8 @@ public class Solution {
        // l1455.isPrefixOfWord("i love eating burger", "burg");
 
         L2554 l2554 = new L2554();
-        l2554.maxCount(new int[]{1, 5, 6}, 6, 6);
+      //  l2554.maxCount(new int[]{1, 5, 6}, 6, 6);
+      L2490 l2490 = new L2490();
+      System.out.println(l2490.isCircularSentence("leetcode exercises sound delightful"));
     }
 }
