14230b88c06c444aa2fbf2b91a9d6762a6bd1fc5
GVOne-blood <dta150904@gmail.com>
2025-02-28 14:11:46 +0700

    L268

---
+++ L268.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Arrays;
+
+public class L268 {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int l = 0, r = nums.length - 1;
+        int m = (l + r) / 2;
+        while (l < r){
+            m = (l + r)/ 2;
+            if (nums[m] > m) r = m;
+            else l = m + 1;
+        }
+        return l;
+    }
+}
