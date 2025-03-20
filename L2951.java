9ec023d72b924bd823ed439595d38ea1c2bea3a3
GVOne-blood <dta150904@gmail.com>
2025-03-20 09:10:51 +0700

    L2951

---
+++ L2951.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class L2951 {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i < mountain.length - 1; i++)
+            if (mountain[i -1] < mountain[i] && mountain[i] > mountain[i + 1])
+                result.add(i);
+    return result;
+    }
+}
