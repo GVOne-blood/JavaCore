b5115c918ad102092b325b8ab5387ffb3e93f648
GVOne-blood <dta150904@gmail.com>
2025-03-31 14:08:43 +0700

    l976

--- L976.java
+++ L976.java
@@ -5,8 +5,8 @@ public class L976 {
     public int largestPerimeter(int[] A) {
         Arrays.sort(A);
         for (int i = A.length - 3; i >= 0; --i)
-            if (A[i] + A[i+1] > A[i+2])
-                return A[i] + A[i+1] + A[i+2];
+            if (A[i] + A[i + 1] > A[i + 2])
+                return A[i] + A[i + 1] + A[i + 2];
         return 0;
-
+    }
     }
